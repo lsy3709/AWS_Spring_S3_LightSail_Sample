74bf7cde547f59c65bfa1c30227503888acb3d4d
LeeSangYong <lsy3709@gmail.com>
2024-05-28 16:26:22 +0900

    용량제한 10메가 확인 테스트완료

--- src/main/java/com/busanit501/spring_aws_s3/controller/FileController2.java
+++ src/main/java/com/busanit501/spring_aws_s3/controller/FileController2.java
@@ -4,10 +4,12 @@ import com.busanit501.spring_aws_s3.service.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -32,13 +34,17 @@ public class FileController2 {
   }
 
   @PostMapping("/multi-upload")
-  public List<String> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
-    List<String> urlList = List.of(files).stream()
-        .map(s3Service::uploadFile)
+  public ResponseEntity<List<String>> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+   try{
+     List<String> urlList = List.of(files).stream()
+         .map(s3Service::uploadFile)
 //        .map(fileUrl -> "<a href=\"" + fileUrl + "\" target=\"_blank\">View Uploaded File</a>")
-        .collect(Collectors.toList());
-
-    return urlList;
+         .collect(Collectors.toList());
+     return ResponseEntity.ok(urlList);
+   }
+   catch (MaxUploadSizeExceededException e) {
+     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(List.of("One or more files exceed size limit!"));
+   }
   }
 
   @GetMapping("/download")
